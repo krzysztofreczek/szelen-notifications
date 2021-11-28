18f3aed9ad7f1a7cb2da5f78a3ca01f6cc8288fb
fredzellek <anna.serafin3@gmail.com>
2021-11-28 16:38:09 +0100

    Transfrom date string into date format

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,15 @@
 import data
+from datetime import date
 
 
-def analyze_trainings(trainings: [data.TrainingData], user: str) -> str:
+def analyze_trainings(trainings: [data.TrainingData], user: str, last_monday_date: date) -> str:
     count = 0
     for training in trainings:
-        if user == training.user:
+        training_date_in_string_format = training.date
+        training_date_in_date_format = date.fromisoformat(training_date_in_string_format)
+
+        if user == training.user and last_monday_date <= training_date_in_date_format:
             count = count + 1
 
-    return "You have " + str(count) + " training(s)"
+    return "You have " + str(count) + " training(s) this week"
 
