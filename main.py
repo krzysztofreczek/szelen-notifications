90e238da0f43c5458863f13c4421c63875d81646
krzysztofreczek <reczek.krzysztof@gmail.com>
2021-12-02 18:29:21 +0100

    Add exemplary call for "shelen" events

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import http.client
+
 # Miro: https://miro.com/app/board/o9J_lhf0WP8=/
 if __name__ == '__main__':
-    print("Hello World!")
+    # this piece of code sends request to szelen repository and retrieves a chunk of events
+    conn = http.client.HTTPSConnection("raw.githubusercontent.com")
+    payload = ''
+    headers = {}
+    conn.request("GET", "/krzysztofreczek/szelen/master/db/events.0.js", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
